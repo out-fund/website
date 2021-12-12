48d857da3ec33b1172db872867fdba4fb75ec69d
Steven Grgic <grgic.stjepan@gmail.com>
2021-12-12 23:42:35 +0000

    Legal pages updates

---
+++ src/pages/legal/cookie-policy/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const CookiePolicy = () => {
+  return (
+    <main>
+      <title>CookiePolicy</title>
+      <h1>CookiePolicy</h1>
+    </main>
+  )
+}
+
+export default CookiePolicy
---
+++ src/pages/legal/privacy-policy/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const PrivacyPolicy = () => {
+  return (
+    <main>
+      <title>PrivacyPolicy</title>
+      <h1>PrivacyPolicy</h1>
+    </main>
+  )
+}
+
+export default PrivacyPolicy
