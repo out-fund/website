ef9b85c8ba68617ffa903b94165fe14fd7a3e16b
Steven Grgic <grgic.stjepan@gmail.com>
2021-12-12 18:58:55 +0000

    Updated legal folders

---
+++ src/pages/how-funding-works/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const HowFundingWorks = () => {
+  return (
+    <main>
+      <title>Contact</title>
+      <h1>Contact</h1>
+    </main>
+  )
+}
+
+export default HowFundingWorks
