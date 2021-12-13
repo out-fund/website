85112183d9ff4ceef306a5bfab8cab07ee5e65d3
Steven Grgic <grgic.stjepan@gmail.com>
2021-12-13 01:09:56 +0000

    Added other pages

---
+++ src/pages/success-stories/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const SuccessStories = () => {
+  return (
+    <main>
+      <title>SuccessStories</title>
+      <h1>SuccessStories</h1>
+    </main>
+  )
+}
+
+export default SuccessStories
---
+++ src/pages/support/faq/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const FAQ = () => {
+  return (
+    <main>
+      <title>FAQ</title>
+      <h1>FAQ</h1>
+    </main>
+  )
+}
+
+export default FAQ
