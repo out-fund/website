c26d373101c86982fcd74a2eb42a0c37bdc198c0
Steven Grgic <grgic.stjepan@gmail.com>
2022-01-05 09:28:28 +0000

    faq page desktop

--- src/pages/support/faq/index.js
+++ src/pages/support/faq/index.js
@@ -6,6 +6,7 @@ import parse from "html-react-parser"
 import EnLayout from "layouts/en"
 import { HeroSimple, Main } from "./../../../components"
 import T from "./../../../styles/new/typography"
+import { theme } from "./../../../styles/new/theme"
 
 import {
   Accordion,
@@ -24,7 +25,9 @@ const Accordions = ({ qa }) => {
           <AccordionItemHeading>
             <AccordionItemButton>{item.question}</AccordionItemButton>
           </AccordionItemHeading>
-          <AccordionItemPanel>{parse(item.answer)}</AccordionItemPanel>
+          <AccordionItemPanel>
+            <T.Body as="div">{parse(item.answer)}</T.Body>
+          </AccordionItemPanel>
         </AccordionItem>
       ))}
     </Accordion>
@@ -102,47 +105,75 @@ export const query = graphql`
 `
 
 const Title = styled(T.H3)``
-const LinksWrapper = styled.ul``
 
-const ContentWrapper = styled.div``
+const LinksWrapper = styled.ul`
+  background-color: ${theme.color.background.emphesized};
+  display: block;
+  padding: 32px;
+  border-radius: 10px;
+  li {
+    display: block;
+    &:not(:last-child) {
+      margin-bottom: 24px;
+    }
+  }
+  a {
+    color: ${theme.color.button.primary.backgroundColor};
+    text-decoration: none;
+  }
+`
+
+const ContentWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 200px 1fr;
+  gap: 100px;
+  max-width: 970px;
+  margin: 0 auto;
+  padding-top: 80px;
+  h3 {
+    margin-bottom: 24px;
+  }
+`
 const Container = styled.div``
-const QuestionSectionWrapper = styled.div``
+const QuestionSectionWrapper = styled.div`
+  margin-bottom: 80px;
+`
 const MenueWrapper = styled.section``
 
-const FaqWrapper = styled.section``
-//   .accordion {
-//     border: none;
-//     border-radius: 0;
-//   }
-//   .accordion__item {
-//     border-top: 2px solid #f2f6fa !important;
-//   }
-//   .accordion__button {
-//     display: flex;
-//     flex-direction: row-reverse;
-//     align-items: center;
-//     justify-content: flex-end;
-//     padding: 24px 24px;
-//     color: #051734;
-//     font-size: 20px;
-//     letter-spacing: -0.02em;
-//     background-color: #fff;
-//     border-radius: 4px;
-//     user-select: none;
+const FaqWrapper = styled.section`
+  .accordion {
+    border: none;
+    border-radius: 0;
+  }
+  .accordion__item {
+    border-top: 1px solid #bbc7d6 !important;
+  }
+  .accordion__button {
+    display: flex;
+    flex-direction: row-reverse;
+    align-items: center;
+    justify-content: flex-end;
+    padding: 24px 24px;
+    color: #051734;
+    font-size: 20px;
+    letter-spacing: -0.02em;
+    background-color: #fff;
+    border-radius: 4px;
+    user-select: none;
 
-//     :hover {
-//       background-color: #deeeff;
-//     }
-//     &:before {
-//       margin-left: auto;
-//       color: #1a65ba;
-//     }
-//   }
-//   .accordion__panel {
-//     padding: 16px 40px 32px;
-//     color: #1c3654;
-//     line-height: 1.5;
-//     background-color: #fff;
-//     border-bottom: 4px solid #f2f6fa !important;
-//   }
-// `
+    :hover {
+      background-color: #deeeff;
+    }
+    &:before {
+      margin-left: auto;
+      color: #1a65ba;
+    }
+  }
+  .accordion__panel {
+    padding: 16px 40px 32px;
+    color: #1c3654;
+    line-height: 1.5;
+    background-color: #fff;
+    border-bottom: 1px solid #bbc7d6 !important;
+  }
+`
