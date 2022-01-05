5ec1af87db7c9253a1f275e5d329f1419528a130
Steven Grgic <grgic.stjepan@gmail.com>
2022-01-05 17:59:52 +0000

    Blog layout is ok for now

--- src/layouts/blogPostLayout.js
+++ src/layouts/blogPostLayout.js
@@ -1,26 +1,38 @@
 import React from "react"
+import styled from "styled-components"
 import { graphql } from "gatsby"
 import { MDXProvider } from "@mdx-js/react"
 import { MDXRenderer } from "gatsby-plugin-mdx"
-import { S } from "styles"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 import EnLayout from "layouts/en"
-import { HeroSimple } from "components"
+import { HeroSimple, Main } from "./../components"
+import T from "./../styles/new/typography"
 
 const BlogPostLayout = ({ data: { mdx } }) => {
+  console.log(mdx)
   return (
     <EnLayout>
       <MDXProvider
         components={{
-          h1: S.H1,
-          h2: S.H2,
-          h3: S.H3,
+          h1: T.H1,
+          h2: T.H2,
+          h3: T.H3,
+          p: T.Body,
         }}
       >
-        <HeroSimple data={mdx.frontmatter.title} />
-        <main>
-          <MDXRenderer frontmatter={mdx.frontmatter}>{mdx.body}</MDXRenderer>
-        </main>
+        <HeroSimple data={mdx.frontmatter} />
+        <Main>
+          <ImageWrapper>
+            <GatsbyImage
+              image={getImage(mdx.frontmatter.heroImage.src)}
+              alt={mdx.frontmatter.heroImage.alt}
+            />
+          </ImageWrapper>
+          <Article>
+            <MDXRenderer frontmatter={mdx.frontmatter}>{mdx.body}</MDXRenderer>
+          </Article>
+        </Main>
       </MDXProvider>
     </EnLayout>
   )
@@ -28,15 +40,28 @@ const BlogPostLayout = ({ data: { mdx } }) => {
 
 export default BlogPostLayout
 
-export const query = graphql`
-  query BlogPostQuery($postId: String) {
-    mdx(id: { eq: $postId }) {
-      id
-      body
-      frontmatter {
-        title
-      }
-    }
+const ImageWrapper = styled.div`
+  /* margin: 0 auto; */
+  display: flex;
+  justify-content: center;
+  position: relative;
+  top: -64px;
+  img {
+    border-radius: 10px;
+  }
+`
+const Article = styled.article`
+  max-width: 970px;
+  margin: 0 auto;
+  /* margin-top: 120px; */
+  /* padding-bottom: 120px; */
+  h2,
+  h3 {
+    margin-bottom: 16px;
+  }
+  p {
+    margin-bottom: 24px;
+    line-height: 1.6;
   }
 `
 
@@ -52,3 +77,27 @@ export const query = graphql`
 // export default function Layout({ children }) {
 //   return <MDXProvider components={shortcodes}>{children}</MDXProvider>
 // }
+
+export const query = graphql`
+  query BlogPostQuery($postId: String) {
+    mdx(id: { eq: $postId }) {
+      id
+      body
+      frontmatter {
+        title
+        heroImage {
+          alt
+          src {
+            childImageSharp {
+              gatsbyImageData(
+                height: 600
+                jpgOptions: { progressive: true, quality: 90 }
+                transformOptions: { cropFocus: CENTER, fit: COVER }
+              )
+            }
+          }
+        }
+      }
+    }
+  }
+`
