13ce5db595c3f0f7f4df67702e76516caab3d85f
Steven Grgic <grgic.stjepan@gmail.com>
2022-01-11 19:57:20 +0000

    prod dev segment

--- gatsby-config.js
+++ gatsby-config.js
@@ -134,7 +134,7 @@ module.exports = {
         // your segment write key for your production environment
         // when process.env.NODE_ENV === 'production'
         // required; non-empty string
-        prodKey: "vbgfxlp9m23Z1yGNmuO9w5FyM0mcSont",
+        prodKey: "dDBm5JEJmHQMqab1sFnGKarzXjcnw3Ra",
 
         // if you have a development env for your segment account, paste that key here
         // when process.env.NODE_ENV === 'development'
@@ -205,6 +205,7 @@ module.exports = {
         //   2. Use the `delayLoad` option here
         // customSnippet:
         // '!function(){var analytics=window.analytics||[];...;analytics.load("${writeKey}");analytics.page();}}();',
+        // '!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="dDBm5JEJmHQMqab1sFnGKarzXjcnw3Ra";;analytics.SNIPPET_VERSION="4.15.3";analytics.load("${writeKey}");analytics.page();}}();',
       },
     },
   ],
