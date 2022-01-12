e0595435e0225c586c56df5673e92ef092831eb7
Steven Grgic <grgic.stjepan@gmail.com>
2022-01-12 08:36:35 +0000

    Google tag manager script added

--- src/html.js
+++ src/html.js
@@ -1,6 +1,32 @@
 import React from "react"
 import PropTypes from "prop-types"
 
+const GoogleTagManager = () => {
+  return (
+    <>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MB8JNFW');`,
+        }}
+      />
+    </>
+  )
+}
+const GoogleTagManagerNoScript = () => {
+  return (
+    <>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-MB8JNFW"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
+    </>
+  )
+}
+
 const LiveChat = () => {
   return (
     <>
@@ -39,9 +65,11 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <GoogleTagManager />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
+        <GoogleTagManagerNoScript />
         {props.preBodyComponents}
         <div
           key={`body`}
