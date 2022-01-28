f50df2f2e348aaed3d6e6608cf542a9d45b0266f
Steven Grgic <grgic.stjepan@gmail.com>
2022-01-28 09:48:44 +0000

    LiveChat is ON

--- src/html.js
+++ src/html.js
@@ -32,12 +32,12 @@ const LiveChat = () => {
     <>
       <script
         dangerouslySetInnerHTML={{
-          __html: `window.__lc = window.__lc || {};window.__lc.license = 13418634;;(function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};!n.__lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e}(window,document,[].slice))`,
+          __html: `window.__lc = window.__lc || {}; window.__lc.license = 13340739;;(function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};!n.__lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e}(window,document,[].slice))`,
         }}
       />
       <noscript>
         <a
-          href="https://www.livechatinc.com/chat-with/13418634/"
+          href="https://www.livechatinc.com/chat-with/13340739/"
           rel="nofollow"
         >
           Chat with us
@@ -65,11 +65,11 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        {/* <GoogleTagManager /> */}
+        <GoogleTagManager />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
-        {/* <GoogleTagManagerNoScript /> */}
+        <GoogleTagManagerNoScript />
         {props.preBodyComponents}
         <div
           key={`body`}
@@ -77,7 +77,7 @@ export default function HTML(props) {
           dangerouslySetInnerHTML={{ __html: props.body }}
         />
         {props.postBodyComponents}
-        {/* <LiveChat /> */}
+        <LiveChat />
       </body>
     </html>
   )
