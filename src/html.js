7c0c8e658f714947cafb8aa49e1011e8b04a2f03
Steven Grgic <grgic.stjepan@gmail.com>
2022-02-18 09:56:20 +0000

    Turn off HotJar

--- src/html.js
+++ src/html.js
@@ -123,7 +123,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <Hotjar />
+        {/* <Hotjar /> */}
         <GoogleTagManager />
         {props.headComponents}
       </head>
