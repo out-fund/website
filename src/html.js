3d6e4aab6e07628c52ad8d9b1a04f49a42ad5371
Steven Grgic <grgic.stjepan@gmail.com>
2022-03-17 16:41:26 +0000

    Removed Live chat

--- src/html.js
+++ src/html.js
@@ -38,81 +38,6 @@ const GoogleTagManagerNoScript = () => {
   )
 }
 
-const LiveChat = () => {
-  return (
-    <>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `window.__lc = window.__lc || {}; window.__lc.license = 13340739;;(function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};!n.__lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e}(window,document,[].slice))`,
-        }}
-      />
-      <noscript>
-        <a
-          href="https://www.livechatinc.com/chat-with/13340739/"
-          rel="nofollow"
-        >
-          Chat with us
-        </a>
-        , powered by{" "}
-        <a
-          href="https://www.livechatinc.com/?welcome"
-          rel="noopener nofollow"
-          target="_blank"
-        >
-          LiveChat
-        </a>
-      </noscript>
-    </>
-  )
-}
-// const DecorateUrls = () => {
-//   return (
-//     <>
-//       <script
-//         dangerouslySetInnerHTML={{
-//           __html: `
-//             (function() {
-//               var domainsToDecorate = [
-//                       'out.fund'
-//                   ],
-//                   queryParams = [
-//                       'utm_medium',
-//                       'utm_source',
-//                       'utm_campaign',
-//                       'utm_content',
-//                       'utm_term',
-//                       'referring_URL'
-//                   ]
-//               var links = document.querySelectorAll('a');
-//               for (var linkIndex = 0; linkIndex < links.length; linkIndex++) {
-//                   for (var domainIndex = 0; domainIndex < domainsToDecorate.length; domainIndex++) {
-//                       if (links[linkIndex].href.indexOf(domainsToDecorate[domainIndex]) > -1 && links[linkIndex].href.indexOf('#') === -1) {
-//                           links[linkIndex].href = decorateUrl(links[linkIndex].href);
-//                       }
-//                   }
-//               }
-//               function decorateUrl(urlToDecorate) {
-//                   urlToDecorate = (urlToDecorate.indexOf('?') === -1) ? urlToDecorate + '?' : urlToDecorate + '&';
-//                   var collectedQueryParams = [];
-//                   for (var queryIndex = 0; queryIndex < queryParams.length; queryIndex++) {
-//                       if (getQueryParam(queryParams[queryIndex])) {
-//                           collectedQueryParams.push(queryParams[queryIndex] + '=' + getQueryParam(queryParams[queryIndex]))
-//                       }
-//                   }
-//                   return urlToDecorate + collectedQueryParams.join('&');
-//               }
-//               function getQueryParam(name) {
-//                   if (name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)')).exec(window.location.search))
-//                       return decodeURIComponent(name[1]);
-//               }
-//             })();
-//           `,
-//         }}
-//       />
-//     </>
-//   )
-// }
-
 export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
@@ -143,8 +68,14 @@ export default function HTML(props) {
           dangerouslySetInnerHTML={{ __html: props.body }}
         />
         {props.postBodyComponents}
-        {/* <DecorateUrls /> */}
-        <LiveChat />
+        {/* Website Chat */}
+        <script
+          type="text/javascript"
+          id="hs-script-loader"
+          async
+          defer
+          src="//js-na1.hs-scripts.com/4522529.js"
+        />
       </body>
     </html>
   )
