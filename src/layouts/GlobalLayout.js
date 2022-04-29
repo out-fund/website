a82c05e42be941c61e85418c9b16c833844303a2
Steven Grgic <grgic.stjepan@gmail.com>
2022-04-29 13:44:29 +0100

    Newsbar added

--- src/layouts/GlobalLayout.js
+++ src/layouts/GlobalLayout.js
@@ -1,18 +1,25 @@
 import React from "react"
 import { theme } from "./../styles/new/theme"
 import styled from "styled-components"
-// import GlobalStyle from "./../styles/new/utils/GlobalStyle"
-
-// import "./../styles/new/typography/font.css"
 
 // const GlobalLayout = ({ children, location }) => {
 const GlobalLayout = ({ children, pt }) => {
-  // console.log("test", location)
   return (
     <>
-      {/* <p>Helmet</p> */}
-      {/* <GlobalStyle /> */}
-      <GlobalWrapper pt={pt}>{children}</GlobalWrapper>
+      <GlobalWrapper pt={pt}>
+        <NewsBar>
+          🎉 Outfund scores £115 million to loan over £500 million to e-commerce
+          firms this year
+          <a
+            href="https://tech.eu/2022/04/27/going-all-out-to-change-the-way-online-businesses-raise-funds-lands-ps115-million-in-outfunds-kitty/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Read more
+          </a>
+        </NewsBar>
+        {children}
+      </GlobalWrapper>
     </>
   )
 }
@@ -25,4 +32,19 @@ const GlobalWrapper = styled.div`
     props.pt
       ? `padding-top: 0px;`
       : `padding-top: ${theme.size.navbarHeight}px;`}; ;
+`
+const NewsBar = styled.div`
+  background-color: #0a213b;
+  color: #fff;
+  padding: 8px;
+  text-align: center;
+  font-size: 14px;
+  line-height: 24px;
+  font-weight: 500;
+  a {
+    margin-left: 24px;
+    color: inherit;
+    font-weight: 400;
+    text-decoration: underline;
+  }
 `
