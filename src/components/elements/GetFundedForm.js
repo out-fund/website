1854b299bc7d7b1d1f6c05e05723afb23950d817
Kyle Bolton <hello@kylebolton.me>
2022-05-13 20:28:01 +0100

    [DEV-1717] Less strict regex

--- src/components/elements/GetFundedForm.js
+++ src/components/elements/GetFundedForm.js
@@ -50,7 +50,7 @@ const GetFundedForm = ({ data, language }) => {
         validate={(values) => {
           const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
           const websiteRegex =
-            /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi
+            /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/
 
           const errors = {}
           if (!values.name) {
