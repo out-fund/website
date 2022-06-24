40ad7241a399763e806163086b7828afbfcaa8cd
Steven Grgic <grgic.stjepan@gmail.com>
2022-06-24 17:26:37 +0100

    Thank you, Removed more B2B

---
+++ src/pages/thank-you/index.dk.js
@@ -0,0 +1,48 @@
+import React from "react"
+import styled from "styled-components"
+
+import LangLayout from "../../layouts/au"
+
+import { SeoComponent } from "../../components"
+import T from "../../styles/new/typography"
+import { theme } from "../../styles/new/theme"
+
+const ThankYou = () => {
+  return (
+    <LangLayout simpleNavbar>
+      <SeoComponent
+        title="Tak for din indsendelse"
+        description="Almost there"
+      />
+      <Wrapper>
+        <ContentWrapper>
+          <Title>Tak for din indsendelse</Title>
+          <Description>Vi vender snart tilbage til dig.</Description>
+        </ContentWrapper>
+      </Wrapper>
+    </LangLayout>
+  )
+}
+
+export default ThankYou
+
+const Title = styled(T.H2)`
+  margin-bottom: 8px;
+`
+const Description = styled(T.Body)`
+  margin-bottom: 40px;
+`
+
+const ContentWrapper = styled.div`
+  max-width: 370px;
+  margin: 0 auto;
+  text-align: center;
+`
+
+const Wrapper = styled.div`
+  background-color: ${theme.color.background.emphesized};
+  min-height: 100vh;
+  padding-top: 10vh;
+  padding-left: 24px;
+  padding-right: 24px;
+`
