f87b7b5e05eacd4236ec0b69e558ac5e5c86d083
Louis Lombardi <louis@louislombardi.co.uk>
2022-07-27 16:03:18 +0100

    add page name options

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,13 +5,63 @@ import { initSegment, track } from "@shiggydoodah/segment-events"
 
 const segmentKey = 'SbUYctfcULJBDClnkbSPOkPmfEPwexBU'
 const regions = ['uk', 'au', 'us', 'es', 'fr', 'dk', 'ro', 'cz', 'nl', 'hu', 'it', 'pt', 'de', 'pl', 'se', 'fr-be', 'nl-be']
+const pageNames = [
+  {
+    path: '/',
+    name: 'Homepage'
+  },
+  {
+    path: '/how-funding-works/',
+    name: 'How funding works'
+  },
+  {
+    path: '/how-funding-works/',
+    name: 'How funding works'
+  },
+  {
+    path: '/about-us/',
+    name: 'About us'
+  },
+  {
+    path: "/success-stories/",
+    name: "Success stories"
+  },
+  {
+    path: "/support/faq/",
+    name: "FAQ"
+  },
+  {
+    path: "/get-funded/",
+    name: "Get funded"
+  },
+  {
+    path: "/partners/",
+    name: "Partners"
+  },
+  {
+    path: "/careers/",
+    name: "Careers"
+  },
+  {
+    path: "/legal/cookie-policy/",
+    name: "Cookie policy"
+  },
+  {
+    path: "/legal/privacy-policy/",
+    name: "Privacy policy"
+  }
+]
 
 export const onClientEntry = () => {
   initSegment(segmentKey, {})
 }
 
 export const onRouteUpdate = () => {
-  track.page(regions, 'website');
+  track.page({
+    regions,
+    platform: 'website',
+    pageNames,
+  });
   track.clicks('[data-segment="click"]', regions, 'website');
   track.textEntered('[data-segment="textInput"]', regions, 'website')
   track.optionSelected('[data-segment="option"]', regions, 'website')
