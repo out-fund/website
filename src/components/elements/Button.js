f905f23c54b2d735712cd608cdf1a6e0adf1df1f
Louis Lombardi <louis@louislombardi.co.uk>
2022-09-14 12:20:46 +0100

    change utm cookie name and remove all null and empty values from utm params

--- src/components/elements/Button.js
+++ src/components/elements/Button.js
@@ -21,10 +21,17 @@ const Button = ({
   ...props
 }) => {
   const [utms, setUtms] = useState({})
-  // TEMPORARY - remove this when we have cross-domain utm tracking in segment working!
+
   useEffect(() => {
-    if (Cookie.get("outfund_analytics")) {
-      setUtms(JSON.parse(Cookie.get("outfund_analytics")))
+    if (Cookie.get("outfund_utm")) {
+      const cookieUtms = JSON.parse(Cookie.get("outfund_utm"))
+      let utmsToSet = {}
+      Object.keys(cookieUtms).forEach((key) => {
+        if (cookieUtms[key] != null && cookieUtms[key] !== "") {
+          utmsToSet[key] = cookieUtms[key]
+        }
+      })
+      setUtms(utmsToSet)
     }
   }, [])
 
@@ -39,15 +46,12 @@ const Button = ({
         .join("&")
   }
   // TEMPORARY -
-
   const langKey = useLangProvider()
-
   if (to && langKey !== "en") {
     to = "/" + langKey + to
   }
   const elementType =
     variant === ("navLink" || "footerLink") ? "Hyperlink" : "Button"
-
   return (
     <ButtonWrap
       variant={variant}
@@ -126,8 +130,8 @@ const ButtonWrap = styled.div`
     }
 
     ${({ variant }) =>
-      variant === "primary"
-        ? css`
+    variant === "primary"
+      ? css`
             background-color: ${theme.color.button.primary.backgroundColor};
             color: ${theme.color.white};
             transition: background-color 0.1s ease-in-out;
@@ -137,29 +141,29 @@ const ButtonWrap = styled.div`
               background-color: ${theme.color.button.primary.hover};
             }
           `
-        : variant === "secondary"
+      : variant === "secondary"
         ? css`
             box-shadow: ${(props) =>
-              props.colorVersion === "white"
-                ? `inset 0px 0px 0px 1px ${theme.color.white}`
-                : `inset 0px 0px 0px 1px ${theme.color.button.secondary.color}`};
+            props.colorVersion === "white"
+              ? `inset 0px 0px 0px 1px ${theme.color.white}`
+              : `inset 0px 0px 0px 1px ${theme.color.button.secondary.color}`};
 
             background-color: ${theme.color.button.secondary.backgroundColor};
             color: ${(props) =>
-              props.colorVersion === "white"
-                ? theme.color.white
-                : theme.color.button.secondary.color};
+            props.colorVersion === "white"
+              ? theme.color.white
+              : theme.color.button.secondary.color};
             transition: background-color 0.1s ease-in-out;
             :hover:enabled,
             :focus:enabled {
               background-color: ${(props) =>
-                props.colorVersion === "white"
-                  ? "rgba(255,255,255, 0.2)"
-                  : theme.color.button.secondary.hover};
+            props.colorVersion === "white"
+              ? "rgba(255,255,255, 0.2)"
+              : theme.color.button.secondary.hover};
             }
           `
         : variant === "navLink"
-        ? css`
+          ? css`
             box-shadow: inset 0px 0px 0px 1px transparent;
             background-color: transparent;
             color: ${theme.color.text.headings};
@@ -174,8 +178,8 @@ const ButtonWrap = styled.div`
               background-color: #deeeff;
             }
           `
-        : variant === "footerLink"
-        ? css`
+          : variant === "footerLink"
+            ? css`
             box-shadow: inset 0px 0px 0px 1px transparent;
             background-color: transparent;
             margin-left: -24px;
@@ -194,8 +198,8 @@ const ButtonWrap = styled.div`
               background-color: #deeeff;
             } */
           `
-        : variant === "countrySelector"
-        ? css`
+            : variant === "countrySelector"
+              ? css`
             box-shadow: inset 0px 0px 0px 1px #4f6781;
             background-color: transparent;
             /* margin-left: -24px; */
@@ -231,14 +235,14 @@ const ButtonWrap = styled.div`
               }
             }
           `
-        : variant === "navDropDown"
-        ? css`
+              : variant === "navDropDown"
+                ? css`
             box-shadow: inset 0px 0px 0px 1px transparent;
             /* box-shadow: inset 0px 0px 0px 1px
               ${({ isOpen }) =>
-              isOpen
-                ? theme.color.button.primary.backgroundColor
-                : "transparent"}; */
+                    isOpen
+                      ? theme.color.button.primary.backgroundColor
+                      : "transparent"}; */
 
             background-color: transparent;
             /* color: ${theme.color.button.primary.backgroundColor}; */
@@ -271,20 +275,20 @@ const ButtonWrap = styled.div`
               } */
             }
             ${(props) =>
-              props.isOpen
-                ? css`
+                    props.isOpen
+                      ? css`
                     background-color: #fff;
                     &:after {
                       opacity: 1;
                     }
                   `
-                : ""}
+                      : ""}
           `
-        : ""};
+                : ""};
 
     ${({ size }) =>
-      size === "large"
-        ? css`
+    size === "large"
+      ? css`
             font-size: 16px;
             padding: 0.66666667em 1.77777778em;
             border-radius: 26px;
@@ -299,7 +303,7 @@ const ButtonWrap = styled.div`
               line-height: 24px;
             }
           `
-        : size === "small"
+      : size === "small"
         ? css`
             font-size: 12px;
             padding: 0.57142857em 1.14285714em;
