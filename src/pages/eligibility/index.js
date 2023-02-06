330ab5209a6c5bfe97e9e62afe02975252d47cac
Steven Grgic <grgic.stjepan@gmail.com>
2023-02-06 15:46:37 +0000

    Removed comment in eligibility page

--- src/pages/eligibility/index.js
+++ src/pages/eligibility/index.js
@@ -40,14 +40,13 @@ const Eligibility = ({ location }) => {
               </Description>
               <ButtonWrapper>
                 <Button
-                  // to="/get-funded/"
                   href="https://client.out.fund/signup"
                   variant="primary"
                   size="large"
                   id="cta-get-funded-eligibility-click"
                   data-element-category="Signup CTA"
                 >
-                  Apply now
+                  Apply now test
                 </Button>
               </ButtonWrapper>
             </>
