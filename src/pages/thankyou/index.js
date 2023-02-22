d02e36bf80989274f4834b96184b2f5d8bf4f6b2
Steven Grgic <grgic.stjepan@gmail.com>
2023-02-22 16:26:27 +0000

    Added simple ThankYou page

---
+++ src/pages/thankyou/index.js
@@ -0,0 +1,50 @@
+import React from "react"
+import styled from "styled-components"
+
+import LangLayout from "../../layouts/en"
+
+import { Button, SeoComponent } from "../../components"
+import T from "../../styles/new/typography"
+import { theme } from "../../styles/new/theme"
+
+const ThankYou = () => {
+  return (
+    <LangLayout simpleNavbar>
+      <SeoComponent
+        title="Thank you"
+        description="We'll be in touch shortly."
+      />
+      <Wrapper>
+        <ContentWrapper>
+          <Title>Thank you for letting us know you’re interested!</Title>
+          <Description>
+            Our team will be reaching out shortly to share more information.
+          </Description>
+        </ContentWrapper>
+      </Wrapper>
+    </LangLayout>
+  )
+}
+
+export default ThankYou
+
+const Title = styled(T.H2)`
+  margin-bottom: 8px;
+`
+const Description = styled(T.Body)`
+  margin-bottom: 40px;
+`
+
+const ContentWrapper = styled.div`
+  max-width: 670px;
+  margin: 0 auto;
+  text-align: center;
+`
+
+const Wrapper = styled.div`
+  background-color: ${theme.color.background.emphesized};
+  min-height: 100vh;
+  padding-top: 10vh;
+  padding-left: 24px;
+  padding-right: 24px;
+`
