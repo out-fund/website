ab2bbaedcd980951ef2fc2d1312f3acd7d219c69
Steven Grgic <grgic.stjepan@gmail.com>
2023-06-03 20:18:41 +0100

    Wip

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,8 +2,9 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: "pages",
+      includes: "../includes",
+      layouts: "../includes/layouts",
       output: "dist",
-      layouts: "./../layouts",
     },
   };
 };
