67ed51a36ebe660879bf5c0d0014d4005f5f41d3
Stjepan Grgic <grgic.stjepan@gmail.com>
2023-08-29 17:06:35 +0100

    404 working now

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,9 +2,11 @@ import Link from "next/link"
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>404, Page Not Found</h2>
-      <Link href="/">Return Home</Link>
-    </div>
+    <html>
+      <body>
+        <h2>Page not Found</h2>
+        <Link href="/">Return Home</Link>
+      </body>
+    </html>
   )
 }
