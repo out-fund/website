ec27fdf24a53538fa14fa11e6ed23d90f5df77e1
Stjepan Grgic <grgic.stjepan@gmail.com>
2023-10-04 11:53:18 +0100

    Clients say start

--- components/ClientsSay.tsx
+++ components/ClientsSay.tsx
@@ -1,16 +1,27 @@
+import { Medium, Heading2 } from "@/components/atoms"
+
+import classes from "./ClientsSay.module.scss"
+
 export default function ClientsSay(props: { title: string; clients: any[] }) {
   return (
-    <div>
-      <h2>{props.title}</h2>
-      <div>
-        {props.clients.map((client) => (
-          <div key={client.company}>
-            <div>{client.company}</div>
-            <div>{client.quote}</div>
-            <div>{client.name}</div>
+    <section>
+      <Medium>
+        <div className={classes.clientsSay}>
+          <div className={classes.textWrapper}>
+            <Heading2>{props.title}</Heading2>
           </div>
-        ))}
-      </div>
-    </div>
+
+          <div>
+            {props.clients.map((client) => (
+              <div key={client.company}>
+                <div>{client.company}</div>
+                <div>{client.quote}</div>
+                <div>{client.name}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </Medium>
+    </section>
   )
 }
