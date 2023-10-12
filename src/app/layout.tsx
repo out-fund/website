c9ff5bc660e42a1e8520d4632c75a7ea90226e6c
Stjepan Grgic <grgic.stjepan@gmail.com>
2023-10-12 18:05:47 +0100

    feat: Added fonts and cleared boilerplate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-import "./globals.css"
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
+import cn from "@/utils/cn"
 
-const inter = Inter({ subsets: ["latin"] })
+import "@/styles/globals.css"
+import { worksans } from "@/utils/font"
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={cn(worksans.className)}>
+      <body>{children}</body>
     </html>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        body: ["var(--font-work-sans"],
+        heading: ["var(--font-work-sans"],
+      },
     },
   },
   plugins: [],
