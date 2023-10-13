45b38476bffdfcbbb244f361169ab3d24a28be06
Stjepan Grgic <grgic.stjepan@gmail.com>
2023-10-13 18:23:09 +0100

    Connected homepage with Prismic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,28 @@
-// import Image from "next/image"
+// export default function Home() {
+//   return <main className="">test</main>
+// }
 
-export default function Home() {
-  return <main className="">test</main>
+import { Metadata } from "next"
+// import { SliceZone } from "@prismicio/react"
+
+import { createClient } from "@/prismicio"
+// import { components } from "@/slices"
+
+export default async function Page() {
+  const client = createClient()
+  const page = await client.getSingle("home_page")
+
+  return <main className="">{page.data.title}</main>
+
+  // return <SliceZone slices={page.data.slices} components={components} />
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient()
+  const page = await client.getSingle("home_page")
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+  }
 }
