25080cfdcddce71112717d6307d33c773f4ca398
Stjepan Grgic <grgic.stjepan@gmail.com>
2023-10-20 18:06:31 +0100

    Trust pilot Swiper works

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,50 +2,54 @@ import { createClient } from "@/prismicio"
 import { PrismicNextLink } from "@prismicio/next"
 import Link from "next/link"
 
+import { Wrapper } from "@/components/atoms"
+
 export default async function Footer() {
   const client = createClient()
   const navigation = await client.getSingle("footer")
 
   return (
     <footer>
-      <div className="logo">
-        <Link href="/">Outfund</Link>
-      </div>
+      <Wrapper>
+        <div className="logo">
+          <Link href="/">Outfund</Link>
+        </div>
 
-      <ul>
-        {navigation.data.company_links.map(({ label, link }) => (
-          <li key={label}>
-            <PrismicNextLink field={link}>{label}</PrismicNextLink>
-          </li>
-        ))}
-      </ul>
-      <ul>
-        {navigation.data.funding_links.map(({ label, link }) => (
-          <li key={label}>
-            <PrismicNextLink field={link}>{label}</PrismicNextLink>
-          </li>
-        ))}
-      </ul>
-      <ul>
-        {navigation.data.support_links.map(({ label, link }) => (
-          <li key={label}>
-            <PrismicNextLink field={link}>{label}</PrismicNextLink>
-          </li>
-        ))}
-      </ul>
+        <ul>
+          {navigation.data.company_links.map(({ label, link }) => (
+            <li key={label}>
+              <PrismicNextLink field={link}>{label}</PrismicNextLink>
+            </li>
+          ))}
+        </ul>
+        <ul>
+          {navigation.data.funding_links.map(({ label, link }) => (
+            <li key={label}>
+              <PrismicNextLink field={link}>{label}</PrismicNextLink>
+            </li>
+          ))}
+        </ul>
+        <ul>
+          {navigation.data.support_links.map(({ label, link }) => (
+            <li key={label}>
+              <PrismicNextLink field={link}>{label}</PrismicNextLink>
+            </li>
+          ))}
+        </ul>
 
-      <div>
-        Copyright © {new Date().getFullYear()} Outfund.{" "}
-        {navigation.data.copyright}
-      </div>
-      <div>{navigation.data.fca_statement}</div>
-      <ul>
-        {navigation.data.policy_links.map(({ label, link }) => (
-          <li key={label}>
-            <PrismicNextLink field={link}>{label}</PrismicNextLink>
-          </li>
-        ))}
-      </ul>
+        <div>
+          Copyright © {new Date().getFullYear()} Outfund.{" "}
+          {navigation.data.copyright}
+        </div>
+        <div>{navigation.data.fca_statement}</div>
+        <ul>
+          {navigation.data.policy_links.map(({ label, link }) => (
+            <li key={label}>
+              <PrismicNextLink field={link}>{label}</PrismicNextLink>
+            </li>
+          ))}
+        </ul>
+      </Wrapper>
     </footer>
   )
 }
---
+++ src/components/atoms/index.tsx
@@ -0,0 +1,5 @@
+export { default as Button } from "./Button"
+export { default as Heading } from "./Heading"
+export { default as Link } from "./Link"
+export { default as Logo } from "./Logo"
+export { default as Wrapper } from "./Wrapper"
